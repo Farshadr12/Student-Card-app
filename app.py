b302f9b7537c4223c41816e18700ca124b6f969d
Farshad <Farshadr12@gmail.com>
2024-03-02 21:45:36 +0100

    organised and added database

--- app.py
+++ app.py
@@ -1,33 +1,41 @@
 from PIL import Image, ImageDraw, ImageFont
-import bargen
+import csv
+
 
 # Define image paths and text
 id_txt = "B340003005"
-bargen.bcode_gen(id_txt)
-background_path = "base.png"
+background_path = "src/base.png"
 foreground_path = "profile.jpg"
-code_path = "Mycode.png"
+code_path = "barcodevan.png"
 major_txt = "Data Analytics"
-name_txt = "Farshad Rouzbehi"
-font_path = "font.ttf"  # Replace with your desired font path
+name_txt = "Hadi Hadiiii"
+font_path = "src/font.ttf"
+
+
+database = open('database.csv', 'r')
+csv_reader = csv.reader(database, delimiter=';')
+
+
+for row in csv_reader:
+    print(row[0])
+
 
 # Open images
 background_image = Image.open(background_path)
-background_image = background_image.convert('RGB')
 foreground_image = Image.open(foreground_path)
-#foreground_image = foreground_image.convert ('RGBA')
-barcode_image = Image.open(code_path)
-#barcode_image = barcode_image.convert('RGBA')
-
-print("Background image:", background_image.format, background_image.mode, background_image.size)
-print("Barcode image:", barcode_image.format, barcode_image.mode, barcode_image.size)
 
 
-code_position = (80, 150, 100 , 100)
+# print("Background image:", background_image.format,
+#       background_image.mode, background_image.size)
+# print("Barcode image:", barcode_image.format,
+#       barcode_image.mode, barcode_image.size)
 
-background_image.paste(barcode_image, code_position)
 
-foreground_position = ( 80, 150)
+code_position = (80, 150, 100, 100)
+
+# background_image.paste(barcode_image, code_position)
+
+foreground_position = (80, 150)
 foreground_image = foreground_image.resize((225, 300))
 background_image.paste(foreground_image, foreground_position)
 
@@ -35,6 +43,7 @@ background_image.paste(foreground_image, foreground_position)
 draw = ImageDraw.Draw(background_image)
 font = ImageFont.truetype(font_path, 27)  # Adjust font size as needed
 
+# texts here
 major_position = (600, 400)
 name_position = (600, 310)
 id_position = (600, 360)
@@ -42,7 +51,7 @@ draw.text(name_position, name_txt, fill=(0, 0, 0), font=font, anchor="ls")
 draw.text(id_position, id_txt, fill=(0, 0, 0), font=font, anchor="ls")
 draw.text(major_position, major_txt, fill=(0, 0, 0), font=font, anchor="ls")
 
-result_path = "combined_image.jpg"
+result_path = "output/combined_image.jpg"
 background_image.save(result_path, format="PNG")
 
 
--- bargen.py
+++ bargen.py
@@ -1,7 +1,11 @@
+from barcode import *
 
-def bcode_gen(data= "BS34000405" ):
+
+def bcode_gen(data="https://idp.unicampania.it/idp/Authn/UserPassword"):
     from barcode import Code128
-    from barcode.writer import ImageWriter 
+    from barcode.writer import ImageWriter
     code = Code128(data, writer=ImageWriter())  # Create a Code128 object
-    mycode = code.save("Mycode")
-    return code
+    mycode = code.save("barcode")
+
+
+bcode_gen()
