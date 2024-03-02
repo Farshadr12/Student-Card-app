758bd0808b0678cb010714764a9df4b5be5fbc24
Farshad <Farshadr12@gmail.com>
2024-03-02 22:48:12 +0100

    - Added a time bar - profile pictures are included in the database now. - Default profile picture added in case there is no picture in the database

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from PIL import Image, ImageDraw, ImageFont
 import csv
+from time import sleep
+from tqdm import tqdm
 
-
-# Define image paths and text
-id_txt = "B340003005"
 background_path = "src/base.png"
-foreground_path = "profile.jpg"
 code_path = "barcodevan.png"
-major_txt = "Data Analytics"
-name_txt = "Hadi Hadiiii"
 font_path = "src/font.ttf"
 
 
@@ -16,43 +12,44 @@ database = open('database.csv', 'r')
 csv_reader = csv.reader(database, delimiter=';')
 
 
-for row in csv_reader:
-    print(row[0])
+for i in tqdm(range(100)):
 
+    for row in csv_reader:
+        background_image = Image.open(background_path)
+        if row[3] == True:
+            foreground_path = row[4]
+            foto_user = Image.open(foreground_path)
+        else:
+            foto_default = "src/profiles/default.png"
+            foto_user = Image.open(foto_default)
 
-# Open images
-background_image = Image.open(background_path)
-foreground_image = Image.open(foreground_path)
+        name_user = row[0]
+        id_user = row[1]
+        date_user = row[2]
+        field_user = row[3]
 
+        code_position = (80, 150, 100, 100)
 
-# print("Background image:", background_image.format,
-#       background_image.mode, background_image.size)
-# print("Barcode image:", barcode_image.format,
-#       barcode_image.mode, barcode_image.size)
+        foreground_position = (80, 150)
+        foto_user = foto_user.resize((225, 300))
+        background_image.paste(foto_user, foreground_position)
 
+        draw = ImageDraw.Draw(background_image)
+        font = ImageFont.truetype(font_path, 27)
 
-code_position = (80, 150, 100, 100)
+        field_position = (600, 400)
+        name_position = (600, 310)
+        id_position = (600, 360)
 
-# background_image.paste(barcode_image, code_position)
+        draw.text(name_position, name_user, fill=(
+            0, 0, 0), font=font, anchor="ls")
+        draw.text(id_position, id_user, fill=(0, 0, 0), font=font, anchor="ls")
+        draw.text(field_position, field_user, fill=(
+            0, 0, 0), font=font, anchor="ls")
 
-foreground_position = (80, 150)
-foreground_image = foreground_image.resize((225, 300))
-background_image.paste(foreground_image, foreground_position)
+        result_path = "output/{name} Cart.jpg".format(name=name_user)
+        background_image.save(result_path, format="PNG")
 
+    sleep(0.001)
 
-draw = ImageDraw.Draw(background_image)
-font = ImageFont.truetype(font_path, 27)  # Adjust font size as needed
-
-# texts here
-major_position = (600, 400)
-name_position = (600, 310)
-id_position = (600, 360)
-draw.text(name_position, name_txt, fill=(0, 0, 0), font=font, anchor="ls")
-draw.text(id_position, id_txt, fill=(0, 0, 0), font=font, anchor="ls")
-draw.text(major_position, major_txt, fill=(0, 0, 0), font=font, anchor="ls")
-
-result_path = "output/combined_image.jpg"
-background_image.save(result_path, format="PNG")
-
-
-print(f"Image saved successfully as: {result_path}")
+print(f"Images saved successfully as: {result_path}")
