458dff777b00b1236865559f71218aeaa82d033f
Farshad <Farshadr12@gmail.com>
2024-03-03 00:02:54 +0100

    NEW Version New Changes: - New Background - New Design - Enrolment Date Added - Matricola is placed on the upper right corner for better visibility - Test text added as refrence

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import csv
 from time import sleep
 from tqdm import tqdm
 
-background_path = "src/base.png"
+background_path = "src/card_background.jpg"
 code_path = "barcodevan.png"
-font_path = "src/font.ttf"
+font_path = "src/ARIALBD.TTF"
 
 
 database = open('database.csv', 'r')
@@ -16,6 +16,7 @@ for i in tqdm(range(100)):
 
     for row in csv_reader:
         background_image = Image.open(background_path)
+
         if row[3]:
             foreground_path = row[4]
             foto_user = Image.open(foreground_path)
@@ -28,28 +29,29 @@ for i in tqdm(range(100)):
         date_user = row[2]
         field_user = row[3]
 
-        code_position = (80, 150, 100, 100)
-
-        foreground_position = (80, 150)
+        foreground_position = (145, 150)
         foto_user = foto_user.resize((225, 300))
         background_image.paste(foto_user, foreground_position)
 
         draw = ImageDraw.Draw(background_image)
         font = ImageFont.truetype(font_path, 27)
+        fontID = ImageFont.truetype(font_path, 40)
 
-        field_position = (600, 400)
-        name_position = (600, 310)
-        id_position = (600, 360)
+        field_position = (611+40, 325+20)
+        name_position = (611+40, 265+20)
+        id_position = (770, 50)
+        date_position = (611+40, 385+20)
 
         draw.text(name_position, name_user, fill=(
             0, 0, 0), font=font, anchor="ls")
-        draw.text(id_position, id_user, fill=(0, 0, 0), font=font, anchor="ls")
+        draw.text(id_position, id_user, fill=(
+            255, 255, 255), font=fontID, anchor="ls")
         draw.text(field_position, field_user, fill=(
+            0, 0, 0), font=font, anchor="ls")
+        draw.text(date_position, date_user, fill=(
             0, 0, 0), font=font, anchor="ls")
 
         result_path = "output/{name} Cart.jpg".format(name=name_user)
         background_image.save(result_path, format="PNG")
 
     sleep(0.001)
-
-print(f"Images saved successfully as: {result_path}")
